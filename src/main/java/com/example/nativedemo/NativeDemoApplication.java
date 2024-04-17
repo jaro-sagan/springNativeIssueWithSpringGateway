85b2335dee8a122195bcfc450bc62dce99a8ec96
Jaroslav Sagan <kohan6936@gmail.com>
2024-04-17 10:06:19 +0200

    Update NativeDemoApplication.java

--- src/main/java/com/example/nativedemo/NativeDemoApplication.java
+++ src/main/java/com/example/nativedemo/NativeDemoApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
